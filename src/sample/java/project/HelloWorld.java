9c3ae96cdaed17d489348d9eff9accb4f811c4cf
sainikn <naveen.saini@gmail.com>
2020-08-11 11:58:19 +1000

    Commit(2) -HelloWorld.java

--- src/sample/java/project/HelloWorld.java
+++ src/sample/java/project/HelloWorld.java
@@ -2,5 +2,10 @@ package sample.java.project
 class HelloWorld{
 	public static void main(String args[]){
 		System.out.println("Hello World...");
+                aMethod();
                 }
+ 	public static void aMethod(){
+  		System.out.println("You are awesome!");
+  		}
 }
+
